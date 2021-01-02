ddddf5e9f8a555d14e2d4109b33e109457aae825
Niklas Kasenburg <nkasenburg@gmail.com>
2021-01-02 22:15:21 +0100

    Add bubble with value to sliders.

--- src/diagrams/graph_new.svelte
+++ src/diagrams/graph_new.svelte
@@ -15,27 +15,53 @@ function positive_rate(predictions, threshold) {
 }
 
 // Default threshold for classifier
-let male_threshold = 0.6;
-let female_threshold = 0.6;
+let male_threshold = 0.5;
+let female_threshold = 0.5;
+let male_bubble;
+let female_bubble;
+
+function bubble_position(bubble, threshold) {
+  var newVal = Number(threshold * 100);
+  // Sorta magic numbers based on size of the native UI thumb
+  bubble.style.left = `calc(${newVal}% + (${12 - newVal * 0.2}px))`;
+}
 
 </script>
 
 <div>
- <div>
   <p> Threshold for male super figures: {male_threshold} </p>
  </div>
- <div>
-  <input class="male" type=range bind:value={male_threshold} min=0.0 max=1.0 step=0.01>
- </div>
- <div>
+<div class="slider">
+  <input class="male" type=range bind:value={male_threshold} on:input={bubble_position(male_bubble, male_threshold)} min=0.0 max=1.0 step=0.01>
+  <output class="bubble" bind:this={male_bubble}>{male_threshold}</output>
+</div>
+<div>
   <p> Threshold for female super figures: {female_threshold} </p>
- </div>
- <div>
-  <input class="female" type=range bind:value={female_threshold} min=0.0 max=1.0 step=0.01>
- </div>
+</div>
+<div class="slider">
+  <input class="female" type=range bind:value={female_threshold} on:input={bubble_position(female_bubble, female_threshold)} min=0.0 max=1.0 step=0.01>
+  <output class="bubble female" bind:this={female_bubble}>{female_threshold}</output>
 </div>
 
 <style>
+
+.slider {
+  margin-bottom: 34px;
+}
+
+.bubble {
+  background: #e88f1c;
+  color: white;
+  margin-top: 1px;
+  padding: 2px 10px;
+  position: absolute;
+  border-radius: 4px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.bubble.female {
+  background: #007bff;
+}
 
 input[type=range] {
   -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
