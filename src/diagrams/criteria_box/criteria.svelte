0163d25096bafed9da871d8b87d90ded62e8eae2
Niklas Kasenburg <nkasenburg@gmail.com>
2021-01-14 12:22:22 +0100

    Add example section to criteria box.

--- src/diagrams/criteria_box/criteria.svelte
+++ src/diagrams/criteria_box/criteria.svelte
@@ -3,21 +3,19 @@
 </script>
 
 <div class="criterium">
-
 	<div class="content">
-		<p class="title"><b>{criteriumData.title}</b></p>
+		<h3><b>{criteriumData.title}</h3>
 		<div>{@html criteriumData.description}</div>
 	</div>
-
+	<div class="examples">
+		<p class="heading"><b>Examples</b></p>
+		<div></div>
+	</div>
 </div>
 
 <style>
 
-	.criterium {
-		padding-top: 1em;
-	}
-
-	.title {
+	.heading {
 		margin-bottom: 0.5em;
 		border: none;
 		padding: 0;
